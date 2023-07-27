d7bed04aa8203385f54bf88204b0295ca838e329
dreampioneer <dreampioneer9999@gmail.com>
2023-07-27 20:02:35 +0900

    almost complete

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -30,7 +30,11 @@ const routes: Routes = [
     path: '',
     redirectTo: '/tabs/tab1',
     pathMatch: 'full'
-  }
+  },
+  {
+    path: 'tab1',
+    loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+  },
 ];
 
 @NgModule({
