80611a81438569a6aaee1962fd264edfc0b91293
dreampioneer <dreampioneer9999gmail.com>
2023-07-30 16:48:45 +0900

    working on async

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'second',
+  appName: 'NEXUS(t)',
   webDir: 'www',
   bundledWebRuntime: false
 };
